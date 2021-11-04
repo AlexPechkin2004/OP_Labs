ae6bf8c390c1e62526c3dd61476c9192ed5f682c
Олександр Печковський <35167206+AlexPechkin2004@users.noreply.github.com>
2021-11-04 15:41:43 +0200

    Add files via upload

---
+++ Лабораторна 5/C++/lab5C++.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int a = 0;
+int k = 0;
+
+int main()
+{
+    cout << "Визначення перших 10 п'ятизначних паліндромів, що є простими числами:\n\n";
+    for (k = 10000; k <= 99999; k++)
+    {
+        if (a < 10)
+        {
+            if (((k / 10000) == (k % 10)) && (((k / 1000) % 10) == ((k % 100) / 10)))
+            {
+                int i = 2;
+                while (k % i != 0)
+                {
+                    i++;
+                }
+                if (i == k)
+                {
+                    cout << k << "\n";
+                    a++;
+                }
+            }
+        }
+    }
+}
